5c39ed1157c51d5058e08038657b4de21aa6fb2d
vanderpatrick <patrick123321123@icloud.com>
2022-08-04 11:34:22 +0000

    Create question and set it in the database, setup superadmin

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS=['https://8000-vanderpatrick-django-sv0ocm8b2a2.ws-eu59.gitpod.io']
