f8837a746e208d8e300cf0bca18ce39c6e616f6a
vanderpatrick <patrick123321123@icloud.com>
2022-08-04 13:57:06 +0000

    Add template for detail.html

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -14,11 +14,8 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-        return render(request, 'polls/detail.html', {'question': question})
+   question = get_object_or_404(Question, pk=question_id)
+   return render(request, 'polls/detail.html',{'question':question})
 
 def results(request, question_id):
     response = "you're looking at the results of question %s."
