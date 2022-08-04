c29565e6fa5b7e26b084514f772cd7c2b73e396e
vanderpatrick <patrick123321123@icloud.com>
2022-08-04 11:53:40 +0000

    add urlpaths tho urls.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -3,3 +3,14 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def detail(request, question_id):
+    return HttpResponse("you're lokking at question %s." % question_id)
+
+
+def results(request, question_id):
+    response = "you're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("you're voting on queston %s." % question_id)
