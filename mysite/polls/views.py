bcdb8a6756aed600e4a1f0ffaa4aaa60f47155ed
vanderpatrick <patrick123321123@icloud.com>
2022-08-04 14:00:58 +0000

    Import libriaries to views.py

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
-from . models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from . models import Question, Choice
 from django.template import loader
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
 
 def index(request):
